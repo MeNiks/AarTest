ec0ab187f52e96783acf2b634f6f176fbc82fc8c
Niks <nikhilesh.patve@gmail.com>
2020-07-04 17:21:37 +0530

    List targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,56 +24,6 @@ task("clean", Delete::class) {
     delete = setOf(rootProject.buildDir)
 }
 
-subprojects {
-    val ktlint by configurations.creating
-    dependencies {
-        ktlint("com.github.shyiko:ktlint:0.34.2")
-    }
-
-    tasks.register<JavaExec>("ktlint") {
-        group = "verification"
-        description = "Check Kotlin code style."
-        classpath = ktlint
-        main = "com.pinterest.ktlint.Main"
-        args("--android", "src/**/*.kt", "src/**/*.kts")
-    }
-
-    tasks.register<JavaExec>("ktlintFormat") {
-        group = "formatting"
-        description = "Fix Kotlin code style deviations."
-        classpath = ktlint
-        main = "com.pinterest.ktlint.Main"
-        args("--android", "-F", "src/**/*.kt", "src/**/*.kts")
-    }
-
-    afterEvaluate {
-
-        tasks.named("check") {
-            dependsOn(ktlint)
-        }
-        extensions.configure(BaseExtension::class.java) {
-            lintOptions {
-                xmlReport = true
-                isAbortOnError = false
-            }
-
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_1_8
-                targetCompatibility = JavaVersion.VERSION_1_8
-            }
-        }
-
-        tasks.withType<KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "1.8"
-                freeCompilerArgs = listOf("-progressive")
-            }
-        }
-    }
-
-}
-
-
 allprojects {
     repositories {
         google()
